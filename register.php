8b8934aeba6916a887a2aaac65b52ff1303bd09e
Skiller141 <skiller141@gmail.com>
2018-04-07 11:30:59 +0300

    again

--- register.php
+++ register.php
@@ -1,44 +1,12 @@
 <?php
 require_once('connect.php');
+require_once('functions.php');
 
 if(isset($_POST) & !empty($_POST)){
     $email = mysqli_real_escape_string($conn, $_POST['email']);
     $pwd = md5($_POST['password']);
     $repPwd = md5($_POST['repPassword']);
-    require_once('functions.php');
-    // function createDirAndSession($conn, $email, $pwd, $repPwd){
-    //     if($pwd === $repPwd) {
-    //         $sql = "INSERT INTO users(email, pass) VALUE ('$email', '$pwd')";
-    //         if(mysqli_query($conn, $sql)){
-    //             $smsg = "User Registration successfull";
-    //             mkdir('users/'.$email, 0777, true);
-    //             file_put_contents('users/'.$email.'/index.html', '<!DOCTYPE html>
-    //             <html lang="en">
-    //             <head>
-    //                 <meta charset="UTF-8">
-    //                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    //                 <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    //                 <title>Document</title>
-    //             </head>
-    //             <body>
-                    
-    //             </body>
-    //             </html>');
-    //             // $sql = "SELECT * FROM users WHERE email='$email' AND pass='$pwd'";
-    //             // $result = mysqli_query($conn, $sql);
-    //             // $count = mysqli_num_rows($result);
-    //             session_start();
-    //             $_SESSION['email'] = $email;
-    //             if(isset($_SESSION['email'])){
-    //                 header('location: admin.php');
-    //             }
-    //         } else {
-    //             $fmsg = "User registration failed";
-    //         }
-    //     } else {
-    //         $fmsg = "Password does not match";
-    //     } 
-    // }
+    
     $sql = "SELECT * FROM users";
     if(!mysqli_query($conn, $sql)){
         $sqlCreateTable = "CREATE TABLE users (
